e6de6d374ffa11b193cafe248ac40f2b88152f4d
Anushka Raman <anushkaraman3@gmail.com>
2026-06-01 22:17:22 +0530

    Add Day 1 C programs

---
+++ Day 1/Q2.c
@@ -0,0 +1,16 @@
+//write a program to print multiplication table of given number 
+#include<stdio.h>
+
+int main(){
+    int num,i;
+
+    printf("enter a number:");
+    scanf("%d",&num);
+
+    printf("multiplication table of %d \n",num);
+
+    for(i=1; i<=10; i++){
+        printf("%d * %d = %d\n",num,i,num*i);
+    }
+    return 0;
+}
