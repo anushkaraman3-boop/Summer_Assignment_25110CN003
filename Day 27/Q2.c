a5b26b7b8f20055c20e3ab28f9037a3239464152
Anushka Raman <anushkaraman3@gmail.com>
2026-06-30 21:51:18 +0530

    day 27

---
+++ Day 27/Q2.c
@@ -0,0 +1,34 @@
+//Program to Create Employee Management System
+
+#include <stdio.h>
+
+struct Employee {
+    int id;
+    char name[50];
+    char department[30];
+    float salary;
+};
+
+int main() {
+    struct Employee e;
+
+    printf("Enter Employee ID: ");
+    scanf("%d", &e.id);
+
+    printf("Enter Employee Name: ");
+    scanf("%s", e.name);
+
+    printf("Enter Department: ");
+    scanf("%s", e.department);
+
+    printf("Enter Salary: ");
+    scanf("%f", &e.salary);
+
+    printf("\n----- Employee Details -----\n");
+    printf("ID         : %d\n", e.id);
+    printf("Name       : %s\n", e.name);
+    printf("Department : %s\n", e.department);
+    printf("Salary     : %.2f\n", e.salary);
+
+    return 0;
+}
