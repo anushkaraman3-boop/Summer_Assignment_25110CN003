1daad9ec304c9cf1a2cc32de4c3775bda6cd456d
Anushka Raman <anushkaraman3@gmail.com>
2026-06-30 20:31:09 +0530

    day 22

---
+++ Day 22/Q2.c
@@ -0,0 +1,21 @@
+//Program to Count Words in a Sentence
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[i] != '\0') {
+        if(str[i] == ' ')
+            words++;
+        i++;
+    }
+
+    printf("Number of words = %d", words);
+
+    return 0;
+}
