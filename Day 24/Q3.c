cacda0774191062d9cd051c18f21c6d26a9d1155
Anushka Raman <anushkaraman3@gmail.com>
2026-06-30 21:25:56 +0530

    day 24

---
+++ Day 24/Q3.c
@@ -0,0 +1,37 @@
+//Program to Find Longest Word in a Sentence
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], longest[100], word[100];
+    int i = 0, j = 0, max = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(1) {
+        if(str[i] != ' ' && str[i] != '\0' && str[i] != '\n') {
+            word[j++] = str[i];
+        }
+        else {
+            word[j] = '\0';
+
+            if(j > max) {
+                max = j;
+                strcpy(longest, word);
+            }
+
+            j = 0;
+        }
+
+        if(str[i] == '\0')
+            break;
+
+        i++;
+    }
+
+    printf("Longest Word = %s", longest);
+
+    return 0;
+}
---
+++ Day 24/Q4.c
@@ -0,0 +1,29 @@
+//Program to Remove Duplicate Characters from a String
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j, duplicate;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    printf("String after removing duplicate characters: ");
+
+    for(i = 0; str[i] != '\0'; i++) {
+        duplicate = 0;
+
+        for(j = 0; j < i; j++) {
+            if(str[i] == str[j]) {
+                duplicate = 1;
+                break;
+            }
+        }
+
+        if(duplicate == 0)
+            printf("%c", str[i]);
+    }
+
+    return 0;
+}
