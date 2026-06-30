e87b8b3537ac5ef3381a46aa28738720f0af7038
Anushka Raman <anushkaraman3@gmail.com>
2026-06-30 20:07:12 +0530

    day 19

---
+++ Day 19/Q2.c
@@ -0,0 +1,40 @@
+//Program to Subtract Two Matrices
+
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    int a[r][c], b[r][c], diff[r][c];
+
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    printf("Difference of matrices:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            diff[i][j] = a[i][j] - b[i][j];
+            printf("%d ", diff[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day 19/Q4.c
@@ -0,0 +1,27 @@
+//Program to Find Diagonal Sum of a Matrix
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j, sum = 0;
+
+    printf("Enter the order of square matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        sum += a[i][i];
+    }
+
+    printf("Sum of diagonal elements = %d\n", sum);
+
+    return 0;
+}
