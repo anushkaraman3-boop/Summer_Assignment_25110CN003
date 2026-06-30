bd0974714c3ec47cfce4ba55b34723127d15d254
Anushka Raman <anushkaraman3@gmail.com>
2026-06-30 20:23:52 +0530

    day 21

---
+++ Day 21/Q3.c
@@ -0,0 +1,28 @@
+//Program to Count Vowels and Consonants
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while(str[i] != '\0') {
+        if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
+
+            if(str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U' ||
+               str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+        i++;
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d", consonants);
+
+    return 0;
+}
